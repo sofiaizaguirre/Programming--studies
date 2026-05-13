602ba28edb0a52eee621d082bc90354cbaff9513
sofiaizaguirre <sofiaizaguirre2011@gmail.com>
2026-05-13 10:21:45 -0300

    create variables.dart

---
+++ --dart/variables.dart
@@ -0,0 +1,7 @@
+void main() {
+  String nome = "Sofia Izaguirre";
+  int idade = 22;
+  bool maiorIdade = idade >= 18;
+  print("$nome, bemvindo ao sistema! Você tem $idade");
+  print("$nome, você é maior de idade $maiorIdade. Seu login foi autorizado.");
+}
